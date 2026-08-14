0cafa73c3548c33603822d687380b4d2dceba7f1
Tejas Deshmukh <deshmukhtejas6529@gmail.com>
2026-08-14 22:40:36 +0530

    Time: 14 ms (26.2%), Space: 46.6 MB (16.64%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       if(t.length() != s.length())
+       {
+        return false;
+       } 
+
+       HashMap<Character,Integer> count = new HashMap<>();
+
+       for(char c : s.toCharArray())
+       {
+        count.put(c,count.getOrDefault(c,0)+1);
+       }
+       for(char c : t.toCharArray())
+       {
+        count.put(c,count.getOrDefault(c,0)-1);
+       }
+
+       for(int values : count.values())
+       {
+        if(values != 0)
+        {
+            return false;
+        }
+       
+       }
+        return true;
+    }
+}
